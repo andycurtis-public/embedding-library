cfae0df2546ac7b3f60e338f143db51494e5ab32
Andy Curtis <contactandyc@gmail.com>
2025-08-22 10:56:57 -0400

    moving to scaffold-repo

--- include/embedding-library/int8_embedding_table.h
+++ include/embedding-library/int8_embedding_table.h
@@ -1,6 +1,7 @@
-// SPDX-FileCopyrightText: 2024-2025 Knode.ai
+// SPDX-FileCopyrightText: 2025 Andy Curtis <contactandyc@gmail.com>
+// SPDX-FileCopyrightText: 2024–2025 Knode.ai — technical questions: contact Andy (above)
 // SPDX-License-Identifier: Apache-2.0
-// Maintainer: Andy Curtis <contactandyc@gmail.com>
+
 #ifndef _embed_int8_embedding_table_H
 #define _embed_int8_embedding_table_H
 
